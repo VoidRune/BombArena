84f6d73b408b47017d4c1bfdfbed95ec65bbebff
AnzePecaver <apecaver@gmail.com>
2023-11-08 17:21:01 +0100

    Added arena update and bomb explosion

--- Application.js
+++ Application.js
@@ -27,18 +27,13 @@ var renderer = new Renderer(device, canvas, context);
 
 var renderData = new RenderData();
 var playerBatch = new InstancedBatch();
+var arenaWallBatch = new InstancedBatch();
+var arenaFloorBatch = new InstancedBatch();
+var arenaTombstoneBatch = new InstancedBatch();
+var arenaEnvironmentBatch = new InstancedBatch();
 var batches = [];
 
-export async function Init()
-{
-    let resourceCache = renderer.resourceCache;
-    let fontGenerator = renderer.fontGenerator;
-    let str = await (await fetch("res/shaders/fontFragment.wgsl")).text();
-    fontGenerator.addText(str, 0, 8, 0);
-    
-    await renderer.Initialize();
-
-	let arena = [
+let arena = [
 	[ '#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#' ],
 	[ '#','_','_','_','_','_','_','_','_','_','_','_','_','_','_','#' ],
 	[ '#','_','#','_','#','_','_','#','#','_','_','#','_','#','_','#' ],
@@ -55,7 +50,16 @@ export async function Init()
 	[ '#','_','#','_','#','_','_','#','#','_','_','#','_','#','_','#' ],
 	[ '#','_','_','_','_','_','_','_','_','_','_','_','_','_','_','#' ],
 	[ '#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#' ]];
+let arenaChanged = false
 
+export async function Init()
+{
+    let resourceCache = renderer.resourceCache;
+    let fontGenerator = renderer.fontGenerator;
+    let str = await (await fetch("res/shaders/fontFragment.wgsl")).text();
+    fontGenerator.addText(str, 0, 8, 0);
+    
+    await renderer.Initialize();
 
     let wall = resourceCache.addMesh(await loadMesh('res/meshes/wall.obj'));
     let floor = resourceCache.addMesh(await loadMesh('res/meshes/floor.obj'));
@@ -63,10 +67,7 @@ export async function Init()
     let environment = resourceCache.addMesh(await loadMesh('res/meshes/environment.obj'));
     let texture1 = resourceCache.addTexture(await loadTexture('res/textures/stoneWall.png'));
     let texture2 = resourceCache.addTexture(await loadTexture('res/textures/stoneTiles.png'));
-    var arenaWallBatch = new InstancedBatch();
-    var arenaFloorBatch = new InstancedBatch();
-    var arenaTombstoneBatch = new InstancedBatch();
-    var arenaEnvironmentBatch = new InstancedBatch();
+    
     arenaWallBatch.setMesh(wall);
     arenaWallBatch.setTexture(texture1);
     arenaFloorBatch.setMesh(floor);
@@ -74,6 +75,30 @@ export async function Init()
     arenaTombstoneBatch.setMesh(tombstone);
     arenaEnvironmentBatch.setMesh(environment);
     arenaEnvironmentBatch.addInstance([0, 0, 0]);
+    
+    updateArena(arena)
+
+    batches.push(arenaWallBatch);
+    batches.push(arenaFloorBatch);
+    batches.push(arenaTombstoneBatch);
+    batches.push(arenaEnvironmentBatch);
+
+    playerBatch.setMesh(wall);
+    playerBatch.addInstance([1, 1, 1]);
+    batches.push(playerBatch);
+
+
+    let teapot = resourceCache.addMesh(await loadMesh('res/meshes/teapot.obj'));
+    var batchTeapot = new InstancedBatch();
+    batchTeapot.setMesh(teapot);
+    batchTeapot.addInstance([-15, 10, 10]);
+    batches.push(batchTeapot);
+}
+
+function updateArena() {
+    arenaWallBatch.reset()
+    arenaFloorBatch.reset()
+    arenaTombstoneBatch.reset()
     for(let y = 0; y < arena.length; y++)
     {
         for(let x = 0; x < arena[y].length; x++)
@@ -93,23 +118,53 @@ export async function Init()
             }
         }
     }
-    batches.push(arenaWallBatch);
-    batches.push(arenaFloorBatch);
-    batches.push(arenaTombstoneBatch);
-    batches.push(arenaEnvironmentBatch);
-
-    playerBatch.setMesh(wall);
-    playerBatch.addInstance([1, 1, 1]);
-    batches.push(playerBatch);
-
-
-    let teapot = resourceCache.addMesh(await loadMesh('res/meshes/teapot.obj'));
-    var batchTeapot = new InstancedBatch();
-    batchTeapot.setMesh(teapot);
-    batchTeapot.addInstance([-15, 10, 10]);
-    batches.push(batchTeapot);
-
 }
+
+let gameLoop = setInterval(() => {
+    if(arenaChanged) {
+        updateArena()
+        arenaChanged = false
+    }
+}, 50)
+
+function explodeBomb(coords, radius) {
+    
+    let [x, y] = coords;
+
+    const directions = [
+        [0, -1],
+        [0, 1],
+        [-1, 0],
+        [1, 0]
+    ];
+
+    for (let [dx, dy] of directions) {
+        for (let i = 1; i <= radius; i++) {
+            let newX = x + dx * i;
+            let newY = y + dy * i;
+
+            if (newX < 0 || newX >= arena.length || newY < 0 || newY >= arena[newX].length) {
+                break
+            }
+
+            if (arena[newX][newY] === '#') {
+                break
+            }
+
+            if (arena[newX][newY] === 'T') {
+                arena[newX][newY] = '_';
+                break
+            }
+
+            if (arena[newX][newY] === '_') {
+                continue
+            }
+        }
+    }
+
+    arenaChanged = true;
+}
+
 
 var lastTime = performance.now() / 1000;
 
@@ -121,16 +176,14 @@ export function RenderFrame()
 
     cam.update(dt);
 
-    //playerBatch.reset();
-    //playerBatch.addInstance([cam.position[0], 1, 1]);
-    playerBatch.updateInstance(0, [cam.position[0], 1, 1]);
+    playerBatch.updateInstance(0, [cam.position[0], 1, cam.position[2]]);
 
     renderData.reset();
     renderData.pushMatrix(cam.viewMatrix);
     renderData.pushMatrix(cam.projectionMatrix);
     renderData.pushMatrix(cam.invViewMatrix);
     renderData.pushMatrix(cam.invProjectionMatrix);
-    let lightPos = [Math.sin(time) * 5 + 8, 10, Math.cos(time) * 5 + 8];
+    let lightPos = [10, 10, 10];
     let ortho = mat4.ortho(mat4.create(), -16, 16, -16, 16, -20, 20);
     ortho[5] *= -1;
     renderData.pushMatrix(mat4.multiply(mat4.create(), ortho, mat4.lookAt(mat4.create(), lightPos, [8, 0, 8], [0, -1, 0])));
